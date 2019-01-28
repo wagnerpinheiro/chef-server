8ccbe5453a3be897dd3965c81414cbf61c2a5b04
Wagner Pinheiro <wagner.pinheiro@mandic.net.br>
2019-01-28 11:23:46 -0200

    append chef-server-ctl reconfigure on start of the container

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ sysctl -wq net.ipv6.conf.lo.disable_ipv6=0
 /opt/opscode/embedded/bin/runsvdir-start &
 if [ -f "/etc/opscode/chef_configured" ]
   then
+    echo -e "\nRunning: 'chef-server-ctl reconfigure'. This step will take a few minutes..."
+    # necessario reconfigure no caso de failover, para refazer as configuracoes no systemd
+    chef-server-ctl reconfigure
     echo -e "\nChef Server already configured!\n"
     chef-server-ctl status
   else
